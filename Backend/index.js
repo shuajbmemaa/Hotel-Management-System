472f3baa39401eab7bc2b2b191a7f8f1f6999ea5
shuajbkingu <shuajbmemaa@hotmail.com>
2024-03-21 23:16:31 +0100

    implementimi i cors me metodat ne backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
+app.use(cors({
+    origin:["http:localhost:5173"],
+    methods:["POST","GET","PUT","DELETE"],
+    credentials:true
+}));
 
 
 
