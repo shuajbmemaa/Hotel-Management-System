8cef0ed0b02c041570194da3470ec8b8f2c5a9e3
shuajbkingu <shuajbmemaa@hotmail.com>
2024-03-23 22:20:09 +0100

    Lidhja me databaze SQL

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import multer from 'multer';
 import path from 'path';
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser';
+import mysql from 'mysql'
 
 const app = express();
 app.use(cors({
@@ -40,7 +41,20 @@ const upload=multer({
     storage:storage
 })
 
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"hms"
+})
 
+db.connect(function(err){
+    if(err){
+        console.log("Gabim gjate lidhjes me databaze SQL");
+    }else{
+        console.log("SQL Connected");
+    }
+})
 
 
 app.listen(3002,()=>{
