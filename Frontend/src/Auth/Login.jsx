dda428ea7e775e6e5d01f101d7cc4c126b10fa0d
shuajbkingu <shuajbmemaa@hotmail.com>
2024-03-24 20:42:02 +0100

    Plotsimi i formes ne Login componenten (Insert)

--- Frontend/src/Auth/Login.jsx
+++ Frontend/src/Auth/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [values,setValues]=useState({
+    email:'',
+    password:''
+  });
+
+
   return (
-    <div>Login</div>
+    <div>
+      <h2>Kycuni ne llogarine tuaj</h2>
+      <form >
+        <label htmlFor="email">Email :</label>
+        <input type="email"
+        placeholder='Shkruani emailin tuaj ...'
+        name='email' />
+        <label htmlFor="password">Password :</label>
+        <input type="password"
+        placeholder='Fjalekalimi'
+        name='password' />
+      </form>
+      <Link
+      to="/register">
+        Resgjistrohu
+      </Link>
+    </div>
   )
 }
 
