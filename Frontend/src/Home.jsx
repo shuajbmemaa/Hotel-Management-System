eeff927f71ce97758c261c71c0b9799c7ca6de86
shuajbkingu <shuajbmemaa@hotmail.com>
2024-03-26 21:52:19 +0100

    Kycja ne baze te rolit ne Frontend

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Admin from './Admin';
+import User from './User';
 
 const Home = () => {
+  const [role,setRole]=useState('');
+  const navigate=useNavigate();
+
+  axios.defaults.withCredentials=true;
+
+  useEffect(()=>{
+    axios.get('http://localhost:3002')
+    .then(res=>{
+      if(res.data.valid){
+        setRole(res.data.role)
+      }else{
+        navigate('/login')
+      }
+    })
+    .catch(err=>console.log(err))
+  })
   return (
-    <div>Home</div>
+    <div>
+      {role === "admin" && <Admin/>}
+      {role === "user" && <User/>}
+    </div>
   )
 }
 
