158c75354bce1b0b9bdb495abc8101932bd50173
shuajbkingu <shuajbmemaa@hotmail.com>
2024-04-02 00:40:27 +0200

    zhvendosja e tokenav nga localStorage ne Cookies ne pjesen e Admin(pjesa e log out)

--- Frontend/src/Admin.jsx
+++ Frontend/src/Admin.jsx
@@ -1,10 +1,16 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 import React from 'react'
 
 const Admin = () => {
   const handleLogout=()=>{
     axios.get('http://localhost:3002/logout')
     .then(res =>{
+      window.localStorage.removeItem("userId")
+     // window.localStorage.removeItem("accessToken")
+      //window.localStorage.removeItem("refreshToken")
+      Cookies.remove("accessToken");
+      Cookies.remove("refreshToken");
         window.location.reload();
     })
     .catch(err=>console.log(err))
