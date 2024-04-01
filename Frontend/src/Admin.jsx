99255b10bbf3d44eacac46e24c428032335c1749
Erzaaaaa <erzaa.17@hotmail.com>
2024-04-01 21:33:17 +0200

    Butoni logout per pjesen e adminit

--- Frontend/src/Admin.jsx
+++ Frontend/src/Admin.jsx
@@ -1,8 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 
 const Admin = () => {
+  const handleLogout=()=>{
+    axios.get('http://localhost:3002/logout')
+    .then(res =>{
+        window.location.reload();
+    })
+    .catch(err=>console.log(err))
+}
   return (
-    <div>Admin</div>
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
   )
 }
 
