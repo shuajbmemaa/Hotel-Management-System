4f367719442e3f3caf4f1362a21c53660708bc6d
shuajbkingu <shuajbmemaa@hotmail.com>
2024-04-02 00:41:18 +0200

    importimi i Cookies dhe largimi i tyre pas logout

--- Frontend/src/User.jsx
+++ Frontend/src/User.jsx
@@ -1,10 +1,14 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 import React from 'react'
 
 const User = () => {
   const handleLogout=()=>{
     axios.get('http://localhost:3002/logout')
     .then(res =>{
+      window.localStorage.removeItem("userId")
+      Cookies.remove("accessToken");
+      Cookies.remove("refreshToken");
         window.location.reload();
     })
     .catch(err=>console.log(err))
