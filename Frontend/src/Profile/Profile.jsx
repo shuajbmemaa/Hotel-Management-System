29c3b46cd1f4a37b5e9cbc8ac59d3c2d2a6c5fc8
Erzaaaaa <erzaa.17@hotmail.com>
2024-05-08 04:12:21 +0200

    Modifkimi i pamjes tek profile

--- Frontend/src/Profile/Profile.jsx
+++ Frontend/src/Profile/Profile.jsx
@@ -1,17 +1,27 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './profile.css';
+
 
 const ProfilePage = () => {
   const [profileData, setProfileData] = useState(null);
 
-  useEffect(() => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    return `${day}-${month}-${year}`;
+};
 
+
+  useEffect(() => {
     const fetchProfileData = async () => {
       try {
-        const userId = window.localStorage.getItem("userId"); // Retrieve userId from local storage
-        const response = await axios.get(`http://localhost:3002/api/profile?userId=${userId}`); 
+        const userId = window.localStorage.getItem("userId");
+        const response = await axios.get(`http://localhost:3002/api/profile?userId=${userId}`);
         setProfileData(response.data);
       } catch (error) {
         console.error('Error fetching profile data:', error);
@@ -22,8 +32,8 @@ const ProfilePage = () => {
   }, []);
 
   const maskPassword = (password) => {
-    const maskLength = password.length ;
-    const maskedPassword = '*'.repeat(maskLength) ;
+    const maskLength = password.length;
+    const maskedPassword = '*'.repeat(maskLength);
     return maskedPassword;
   };
 
@@ -33,26 +43,35 @@ const ProfilePage = () => {
 
   return (
     <div className="profile-container">
-      <h1 className="profile-heading">Profile</h1>
-      <ul className="profile-list">
-        <li className="profile-item">
-        <img src={`http://localhost:3002/images/${profileData ? profileData.img_url : ''}`} alt="" className='useret_image' />
-        </li>
-        <li className="profile-item">
-          <strong>ID:</strong> {profileData.id}
-        </li>
-        <li className="profile-item">
-          <strong>Name:</strong> {profileData.name}
-        </li>
-        <li className="profile-item">
-          <strong>Email:</strong> {profileData.email}
-        </li>
-        <li className="profile-item">
-          <strong>Password:</strong>
-          <span>{maskPassword(profileData.password)}</span>
-        </li>
-      </ul>
-    </div>
+  <div className="cover-photo">
+    <img src={`http://localhost:3002/images/${profileData ? profileData.img_url : ''}`} alt="" className='profile' />
+  </div>
+  <div className="profile-name">{profileData.name}</div>
+  <div className="profile-role">{profileData.role}</div>
+  <div className="details">My details</div>
+  <ul className="profile-list">
+    <li className="profile-item">
+      <div className="profile-item-wrapper">
+      <i class="bi bi-envelope"></i>
+        <div className="profile-email">{profileData.email}</div>
+      </div>
+    </li>
+    <li className="profile-item">
+      <div className="profile-item-wrapper">
+      <i class="bi bi-person-standing"></i><i class="bi bi-person-standing-dress"></i>
+        <div className="profile-gender">{profileData.gender}</div>
+      </div>
+    </li>
+    <li className="profile-item">
+      <div className="profile-item-wrapper">
+      <i class="bi bi-calendar-event"></i>
+        <div className="profile-dateBirth">{formatDate(profileData.date_of_birth)}</div>
+      </div>
+    </li>
+  </ul>
+</div>
+
   );
 };
+
 export default ProfilePage;
