fd2969b3640e7904a915a8604f9dff29cf1247ba
shuajbkingu <shuajbmemaa@hotmail.com>
2024-05-23 19:02:43 +0200

    Call mongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,12 @@ import postRoutes from './Routes/routesPost.js';
 import getRoutes from './Routes/routesGet.js';
 import deleteRoutes from './Routes/routesDelete.js';
 import putRoutes from './Routes/routesPut.js';
+import connectDB from './MongoDB/connection.js';
 
 
 const app = express();
 app.use(cors({
-  origin:["http://localhost:5173"],
+    origin: ["http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
@@ -21,21 +22,36 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-    secret:'secret',
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        secure:false,
-        maxAge:1000*60*60*24
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24
     }
 }))
 
- 
-app.use('/',postRoutes);
-app.use('/',getRoutes);
-app.use('/',deleteRoutes);
-app.use('/',putRoutes);
 
-app.listen(3002,()=>{
+app.use('/', postRoutes);
+app.use('/', getRoutes);
+app.use('/', deleteRoutes);
+app.use('/', putRoutes);
+
+const mongoURI = "mongodb+srv://shuajbmema:shuajb@hotel.8cdjodu.mongodb.net/?retryWrites=true&w=majority&appName=Hotel"
+
+const start = async () => {
+    try {
+        await connectDB(mongoURI);
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("Error", error)
+    }
+}
+
+start();
+
+
+
+app.listen(3002, () => {
     console.log("Server is running ");
 })
