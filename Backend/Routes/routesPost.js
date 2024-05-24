c6028859dc5bbc3cb857d3d90742e414677a79fe
Erzaaaaa <erzaa.17@hotmail.com>
2024-05-24 17:27:28 +0200

    Back warning kur numri i dhomes ekziston gjate insertimit te nje dhome te re

--- Backend/Routes/routesPost.js
+++ Backend/Routes/routesPost.js
@@ -3,7 +3,7 @@ import db from '../DB/db.js'
 import jwt from 'jsonwebtoken'
 import path from 'path';
 import multer from 'multer';
-import Chat from '../MongoDB/models/Chat.js';
+//import Chat from '../MongoDB/models/Chat.js';
 
 
 const postRoutes = express.Router();
@@ -133,6 +133,7 @@ postRoutes.post('/shtonjeFloor', (req, res) => {
 });
 
 
+
 postRoutes.post('/krijoDhome', upload.single('image'), (req, res) => {
   const sql = "Insert into room_types (`title`,`short_code`,`base_occupancy`,`higher_occupancy`,`extra_bed`,`kids`,`amenties_id`,`base_price`,`extra_bed_price`,`image`) VALUES (?)"
   const values = [
@@ -157,17 +158,25 @@ postRoutes.post('/krijoDhome', upload.single('image'), (req, res) => {
 
 
 postRoutes.post('/shtoRoom', (req, res) => {
-  const sql = "Insert into room (`floor_id`,`room_type_id`,`room_number`) VALUES (?)"
-  const values = [
-    req.body.floor_id,
-    req.body.room_type_id,
-    req.body.room_number
-  ]
-  db.query(sql, [values], (err, result) => {
-    if (err) return res.json(err)
-    return res.json({ Status: "Success" })
-  })
-})
+  const{floor_id,room_type_id,room_number}=req.body;
+  const checkRoomNumber="Select * from room where room_number=?"
+  db.query(checkRoomNumber,[room_number],(err,result) =>{
+    if(err){
+      return res.status(500).json({message:"Error"})
+    }
+    if(result.length>0){
+      return res.status(400).json({message:"Ky numer i dhomes ekziston!!"})
+    }else{
+      const sql = "INSERT INTO room (floor_id, room_type_id, room_number) VALUES (?, ?, ?)"
+  db.query(sql, [floor_id,room_type_id,room_number], (err, result) => {
+    if (err) {
+      return res.status(500).json({message:"Error"});
+    }
+     return res.status(201).json({message:"Dhoma u shtua me sukses!"});
+  });
+}
+});
+});
 
 postRoutes.post('/createHall', (req, res) => {
   const sql = "Insert into hall (`floor_id`,`hall_type_id`,`hall_number`) VALUES (?)"
@@ -197,23 +206,23 @@ postRoutes.post('/shtoService', (req, res) => {
   })
 })
 
-postRoutes.post('/chat', async (req, res) => {
-  const { question, response } = req.body;
-  try {
-    const chat = await Chat.findOne({ question });
-    if (chat) {
-      chat.response = response;
-      await chat.save();
-      res.json({ message: 'Përgjigja u përditësua me sukses.' });
-    } else {
-      const newChat = new Chat({ question, response });
-      await newChat.save();
-      res.json({ message: 'Përgjigja u ruajt me sukses.' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+// postRoutes.post('/chat', async (req, res) => {
+//   const { question, response } = req.body;
+//   try {
+//     const chat = await Chat.findOne({ question });
+//     if (chat) {
+//       chat.response = response;
+//       await chat.save();
+//       res.json({ message: 'Përgjigja u përditësua me sukses.' });
+//     } else {
+//       const newChat = new Chat({ question, response });
+//       await newChat.save();
+//       res.json({ message: 'Përgjigja u ruajt me sukses.' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
 
 
 
