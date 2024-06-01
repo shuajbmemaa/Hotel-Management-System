4445a9612248ec9d31f41d3272fe5896bf45fafa
erjona00 <119108914+erjona00@users.noreply.github.com>
2024-06-01 13:09:36 +0200

    Add files via upload
    Sherbimet ne user

--- Frontend/src/Main/Sherbimet.jsx
+++ Frontend/src/Main/Sherbimet.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Sherbimet = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3002/getServices')
+      .then(res => {
+        if (res.data.Status === "Success") {
+          setServices(res.data.Result);
+        } else {
+          toast.error("Error");
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
   return (
-    <div>Sherbimet</div>
+    <div>
+      <div className="navbar">
+        <h1>Lotus</h1>
+  
+        </div>
+        <div className="hotel-services">
+        <h2>Shërbimet Tona</h2>
+        <ul className="service-list">
+          {services.map(service => (
+            <li key={service.id}>
+              <h3>{service.title}</h3>
+              <p>Tipi i dhomës: {service.roomType}</p>
+              <p>Çmimi: {service.price} €</p>
+            </li>
+          ))}
+        </ul>
+        <Link to='/'>Kthehu</Link>
+      </div>
+
+
+        </div>
   )
 }
 
