963818cb0d3f43bd213e3bab5251deb1abe5fd33
shuajbkingu <shuajbmemaa@hotmail.com>
2024-08-23 22:08:27 +0200

    Display booked rooms on specific dates in calendar and add PDF download functionality

--- Frontend/src/MyCalendar.jsx
+++ Frontend/src/MyCalendar.jsx
@@ -4,6 +4,9 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import axios from 'axios';
 import {Tooltip} from 'react-tooltip';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { FaRegFilePdf } from "react-icons/fa6";
 
 const localizer = momentLocalizer(moment);
 
@@ -62,24 +65,53 @@ const MyCalendar = () => {
     };
   };
 
+  const handleDownloadPDF = () => {
+    html2canvas(document.querySelector(".rbc-calendar")).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = 210;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+
+      let position = 0;
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pdf.internal.pageSize.height;
+
+      while (heightLeft >= 0) {
+        position -= pdf.internal.pageSize.height;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pdf.internal.pageSize.height;
+      }
+
+      pdf.save('calendar.pdf');
+    });
+  };
+
+
   return (
-    <div style={{ height: '500pt' }}>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        eventPropGetter={eventStyleGetter}
-        components={{
-          event: ({ event }) => (
-            <div data-tip={event.tooltip} style={{ padding: '5px' }}>
-              {event.title}
-            </div>
-          )
-        }}
-      />
-      <Tooltip />
+    <div>
+      <div style={{ height: '420pt' }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          eventPropGetter={eventStyleGetter}
+          components={{
+            event: ({ event }) => (
+              <div data-tip={event.tooltip} style={{ padding: '5px' }}>
+                {event.title}
+              </div>
+            )
+          }}
+        />
+        <Tooltip />
+      </div>
+      <button onClick={handleDownloadPDF}>Download <FaRegFilePdf style={{ fontSize: '30px', marginLeft: '8px' }}/></button>
     </div>
   );
 };
