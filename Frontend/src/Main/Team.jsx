786161e1711130481ae893e5113eeeb45c6da5a1
shuajbkingu <shuajbmemaa@hotmail.com>
2024-08-25 18:21:59 +0200

    Added the navbar view to Team Component

--- Frontend/src/Main/Team.jsx
+++ Frontend/src/Main/Team.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Navbar from '../Layout/Navbar'
+import axios from 'axios';
 
 const Team = () => {
+
+  const handleLogout = () => {
+    axios.get('http://localhost:3002/logout')
+      .then(res => {
+        window.localStorage.removeItem("userId");
+        window.localStorage.removeItem("role");
+        Cookies.remove("accessToken");
+        Cookies.remove("refreshToken");
+        window.location.reload();
+      })
+      .catch(err => console.log(err));
+  };
   return (
-    <div>Team</div>
+    <div>
+      <Navbar onLogout={handleLogout}/>
+    </div>
   )
 }
 
