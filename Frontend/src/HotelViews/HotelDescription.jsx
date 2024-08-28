21fa10e5eb3de0694d43e3f1418665c0d71d55d5
shuajbkingu <shuajbmemaa@hotmail.com>
2024-08-29 00:49:30 +0200

    Modified HotelDescription component and added smooth scroll on buttons

--- Frontend/src/HotelViews/HotelDescription.jsx
+++ Frontend/src/HotelViews/HotelDescription.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import img from '../assets/HoteliAI.jpg';
+import './hoteldescription.css';
+import hotelView from '../assets/borchez.jpeg'
+import airportTaxi from '../assets/arp.png'
+import wifi from '../assets/wifi.jpg'
+import pool from '../assets/swimming-pool.png'
+import breakfast from '../assets/breakfast.png'
 
 const HotelDescription = () => {
   return (
-    <div className="hotel-description">
-      <div className="description-text">
-        <h2>Rreth Hotelit Tonë</h2>
-        <p>
-          Hoteli ynë ofron shërbime të jashtëzakonshme dhe akomodime të shkëlqyera për të gjithë vizitorët tanë.
-          Me një vendndodhje të përsosur dhe stafin tonë miqësor, ne sigurohemi që qëndrimi juaj të jetë sa më i këndshëm.
-        </p>
+    <div className="hotel-landing-page">
+      <div className="hero-section">
+      <img src={hotelView} alt="Hotel Image" className="hero-image" />
+        <div className="overlay">
+          <div className="hero-content">
+            <h1>CURATING THE WORLD'S BEST LUXURY</h1>
+            <h1>HOTEL EXPERIENCE FOR YOU</h1>
+            <p>Experience True Luxury and Elegance Without Compromise</p>
+            <div className="hero-buttons">
+              <button className="explore-btn" onClick={() => document.getElementById('hotel-services').scrollIntoView({ behavior: 'smooth' })}>Explore</button>
+              <button className="book-now-btn" onClick={() => document.getElementById('hotel-room-types').scrollIntoView({ behavior: 'smooth' })}>Rooms</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="description-image">
-        <img src={img} alt="Hotel Image" />
+      <div className="amenities">
+        <div className="amenity">
+          <img src={airportTaxi} alt="Airport Taxi" />
+          <p>Airport Taxi</p>
+        </div>
+        <div className="amenity">
+          <img src={wifi} alt="Free Wifi" />
+          <p>Free Wifi</p>
+        </div>
+        <div className="amenity">
+          <img src={pool} alt="Swimming Pool" />
+          <p>Swimming Pool</p>
+        </div>
+        <div className="amenity">
+          <img src={breakfast} alt="Breakfast" />
+          <p>Breakfast</p>
+        </div>
       </div>
     </div>
   );
